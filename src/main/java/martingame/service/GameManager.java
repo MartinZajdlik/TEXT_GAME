cff49c27dbcedec9434f21bdb24e03c377294907
MartinZajdlik <m.zajdlik@seznam.cz>
2025-01-05 21:16:31 +0100

    add game menu

--- src/main/java/martingame/service/GameManager.java
+++ src/main/java/martingame/service/GameManager.java
@@ -1,34 +1,75 @@
 package martingame.service;
 
-import martingame.ability.Ability;
 import martingame.ability.HeroAbilityManager;
+import martingame.constant.Constant;
 import martingame.domain.Hero;
 import martingame.utility.InputUtils;
 import martingame.utility.PrintUtils;
-
-import java.util.Map;
 
 public class GameManager {
 
     private final Hero hero;
     private final HeroAbilityManager heroAbilityManager;
+    private int currentLevel;
 
-    public GameManager(){
+
+    public GameManager() {
         this.hero = new Hero("");
         this.heroAbilityManager = new HeroAbilityManager(this.hero);
+        this.currentLevel = Constant.INITIAL_LEVEL;
     }
 
-    public void startGame () {
+    public void startGame() {
+        this.initGame();
+
+        while (this.currentLevel <= 5) {
+            System.out.println("0. Fight " + "Level " + this.currentLevel);
+            System.out.println("1. Upgrade abilities (" + hero.getHeroAvailablePoints() + " points to spend)");
+            System.out.println("2. Save Game");
+            System.out.println("3. Exit game");
+
+            final int choice = InputUtils.readInt();
+            switch (choice) {
+                case 0 -> {
+                    // TODO fight
+                    this.currentLevel++;
+                }
+                case 1 -> {
+                    // TODO upgrade abilities
+                }
+                case 2 -> {
+                    // TODO save game
+                }
+                case 3 -> {
+                    System.out.println("Are you sure?");
+                    System.out.println("0. No");
+                    System.out.println("1. Yes");
+                    final int exitChoice = InputUtils.readInt();
+                    if (exitChoice == 1) {
+                        System.out.println("Bye!");
+                        return;
+                    }
+                    System.out.println("Continuing...");
+                    PrintUtils.printDivider();
+                }
+                default -> System.out.println("Invalid input");
+
+            }
+
+        }
+        System.out.println("You have won the game! Congratulations!");
+
+    }
+    private void initGame () {
         System.out.println("Welcome to GLADIATOR GAME");
         System.out.println("Enter your name: ");
         final String name = InputUtils.readString();
         this.hero.setName(name);
-        System.out.println("Hello "+hero.getName()+". Let's start the game!");
+        System.out.println("Hello " + hero.getName() + ". Let's start the game!");
         PrintUtils.printDivider();
         System.out.println("Your abilities: ");
         PrintUtils.printAbilities(hero);
         PrintUtils.printDivider();
         this.heroAbilityManager.spendHeroAvailablePoints();
     }
-
 }
